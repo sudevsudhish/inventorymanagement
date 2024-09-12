5c4057e580d7616a47fdd32b9fa4e3e4dc21eb40
sudevsudhish <sudevsudhish@gmail.com>
2024-09-12 17:28:06 +0530

    added images

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -82,8 +82,8 @@ const Sidebar = () => {
         }`}
       >
         <Image
-          src="https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/logo.png"
-          alt="edstock-logo"
+          src="https://s3-inventorymanagement-sudev.s3.ap-south-1.amazonaws.com/logo.png"
+          alt="sdinventory-logo"
           width={27}
           height={27}
           className="rounded w-8"
